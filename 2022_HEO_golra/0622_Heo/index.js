cb5356fc689b24e7cd9f2575a44146623703dca5
yoona1110 <gjdbsdk18@icloud.com>
2022-06-20 15:14:05 +0900

    캐러셀 오류 고침

---
+++ 2022_HEO_golra/0622_Heo/index.js
@@ -0,0 +1,19 @@
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const carousel = document.querySelector('.carousel');
+
+let index = 0;
+
+prevButton.addEventListener('click', () => {
+   if (index === 0) index += 3;
+   index -= 1;
+   
+   carousel.style.transform = `translate3d(-${215 * index}px, 0, 0)`;
+});
+
+nextButton.addEventListener('click', () => {
+   if (index === 2) index -= 3;
+   index += 1;
+   
+   carousel.style.transform = `translate3d(-${215 * index}px, 0, 0)`;
+});

1cf037b1ccea1a4f3aea40afd20a336dd1c74118
yoona1110 <gjdbsdk18@icloud.com>
2022-09-16 02:25:55 +0900

    중간점검

---
+++ 2022_HEO_golra/0819&0828_Heo/html&css_eatMenu/coffee_menu/menu_option.js
@@ -0,0 +1,49 @@
+// 음료 개수 +- 버튼 설정
+const minus = document.querySelector(".minus");
+const plus = document.querySelector(".plus");
+var countNum = document.querySelector(".menu_number");
+var price = document.querySelector(".menu_price");
+
+minus.addEventListener('click', () => {
+    countNum.innerHTML--;
+
+    if (countNum.innerHTML <= 0) {
+        countNum.innerHTML = 0;
+    }
+});
+plus.addEventListener('click', () => {
+    countNum.innerHTML++;
+    price.innerHTML + 1500;
+});
+
+// 샷 추가 +- 버튼 설정
+const ad_minus = document.querySelector(".add_minus");
+const ad_plus = document.querySelector(".add_plus");
+var ad_countNum = document.querySelector(".add_number");
+
+ad_minus.addEventListener('click', () => {
+    ad_countNum.innerHTML--;
+
+    if (ad_countNum.innerHTML <= 0) {
+        ad_countNum.innerHTML = 0;
+    }
+});
+ad_plus.addEventListener('click', () => {
+    ad_countNum.innerHTML++;
+});
+
+// 시럽 추가 +- 버튼 설정
+const sy_minus = document.querySelector(".syrup_minus");
+const sy_plus = document.querySelector(".syrup_plus");
+var sy_countNum = document.querySelector(".syrup_number");
+
+sy_minus.addEventListener('click', () => {
+    sy_countNum.innerHTML--;
+
+    if (sy_countNum.innerHTML <= 0) {
+        sy_countNum.innerHTML = 0;
+    }
+});
+sy_plus.addEventListener('click', () => {
+    sy_countNum.innerHTML++;
+});
--- 2022_HEO_golra/0819&0828_Heo/html&css_eatMenu/eat.js
+++ 2022_HEO_golra/0819&0828_Heo/html&css_eatMenu/eat.js
@@ -1,4 +1,17 @@
-function openMenu() {
-    var uri = './coffee_menu/coffee_Americano.html';
-    window.open(uri, '_blank', 'width=430px, height=600px, left=535px, top=150px, toolbars=no, scrollbars=no');
-  }
+const prev_menu = document.querySelector('.prev');
+const next_menu = document.querySelector('.next');
+const menu = document.querySelectorAll('.ch_coffee');
+
+let index = 0;
+
+prev_menu.addEventListener('click', () => {
+  if (index === 0) index += 1;
+  index -= 1;
+  menu.style.transform = `translate3d(-${220 * index}px, 0, 0)`;
+});
+
+// 메뉴 선택창에서 클릭한 메뉴 옵션창 띄우기
+function openMenu() {     
+  var uri = './coffee_menu/coffee_Americano.html';
+  window.open(uri, '_blank', 'width=430px, height=600px, left=535px, top=150px, toolbars=no, scrollbars=no');
+}
